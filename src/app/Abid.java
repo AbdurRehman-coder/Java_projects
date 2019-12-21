4cb76f2d4ecd82e82b8681e4f164a9b02b79dca0
ABDUR REHMAN <52750545+AbdurRehman-coder@users.noreply.github.com>
2019-12-21 23:38:14 +0500

    checking
    just chheccking

--- src/app/Abid.java
+++ src/app/Abid.java
@@ -6,11 +6,15 @@ public class Abid extends Student
         String n = new String();
         n = "i am Abid Afghan";
         System.out.println("name: " + n);
+	System.out.println("i am also comrade"); 
+	
+
     }
     public void address()
     {
         String a = new String();
         a = "i am from upper Dir";
+	System.out.println("from pukhtoonistan");
         System.out.println("Address: " + a);
     }
 	@Override
